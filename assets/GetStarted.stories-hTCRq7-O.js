0caffdefb34e03cab49e2645de8b9a04096efc31
tejas-encora <tejas.monteverdi@encora.com>
2023-12-18 09:01:00 -0500

    New Build

---
+++ assets/GetStarted.stories-hTCRq7-O.js
@@ -0,0 +1,17 @@
+import"./chunk-HLWAVYOI-rWy3NJ_P.js";import{M as a}from"./index-ojCAzbbI.js";import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{useMDXComponents as o}from"./index-_VGcpBFS.js";import"./iframe-Rka168tK.js";import"../sb-preview/runtime.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./chunk-ZGA76URP-Uk8hDnuP.js";import"./index-ogXoivrg.js";import"./index-yyIE76pf.js";import"./index-PPLHz8o0.js";function s(r){const t=Object.assign({h1:"h1",p:"p",a:"a",h2:"h2",h3:"h3",code:"code",strong:"strong",pre:"pre"},o(),r.components);return e.jsxs(e.Fragment,{children:[e.jsx(a,{title:"Get Started",parameters:{previewTabs:{canvas:{hidden:!0}}}}),`
+`,e.jsx(t.h1,{id:"ioui-component-library",children:"ioUI Component Library"}),`
+`,e.jsx(t.p,{children:e.jsx(t.a,{href:"https://www.npmjs.com/package/@encoradxp/mat-lib-test",target:"_blank",rel:"nofollow noopener noreferrer",children:"NPM Package"})}),`
+`,e.jsx(t.h2,{id:"get-started",children:"Get Started"}),`
+`,e.jsx(t.h3,{id:"1-install",children:"1. Install"}),`
+`,e.jsx(t.p,{children:e.jsx(t.code,{children:"npm install @encoradxp/mat-lib-test"})}),`
+`,e.jsxs(t.p,{children:[e.jsx(t.strong,{children:"Note"}),": Currently work in progress: Filter Chips, DataTables. ",e.jsx(t.a,{href:"?path=/docs/ui-labs--docs",children:"Go to labs"}),"."]}),`
+`,e.jsx(t.h3,{id:"2-basic-usage",children:"2. Basic Usage"}),`
+`,e.jsx(t.pre,{children:e.jsx(t.code,{className:"language-jsx",children:`<script setup>
+import IOButtonFilled from '@encoradxp/mat-lib-test
+<\/script>
+
+<template>
+    <IOButtonFilled>Click things</IOButtonFilled>
+</template>
+ 
+`})})]})}function i(r={}){const{wrapper:t}=Object.assign({},o(),r.components);return t?e.jsx(t,{...r,children:e.jsx(s,{...r})}):s(r)}const p=()=>{throw new Error("Docs-only story")};p.parameters={docsOnly:!0};const n={title:"Get Started",parameters:{previewTabs:{canvas:{hidden:!0}}},tags:["stories-mdx"],includeStories:["__page"]};n.parameters=n.parameters||{};n.parameters.docs={...n.parameters.docs||{},page:i};const w=["__page"];export{w as __namedExportsOrder,p as __page,n as default};
