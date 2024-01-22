1bcf7db03a8311ebd125b22c6aa5920c76b2cb4f
tejas-encora <tejas.monteverdi@encora.com>
2024-01-22 14:50:52 -0500

    New Build

---
+++ assets/ChangeLog-WNtihfFs.js
@@ -0,0 +1,82 @@
+import{j as e}from"./jsx-runtime-vNq4Oc-g.js";import{useMDXComponents as t}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";function o(n){const r=Object.assign({h1:"h1",p:"p",a:"a",h3:"h3",ul:"ul",li:"li",strong:"strong",h2:"h2"},t(),n.components);return e.jsxs(e.Fragment,{children:[e.jsx(r.h1,{id:"changelog",children:"Changelog"}),`
+`,e.jsxs(r.p,{children:["All notable changes to this project will be documented in this file. See ",e.jsx(r.a,{href:"https://github.com/conventional-changelog/standard-version",target:"_blank",rel:"nofollow noopener noreferrer",children:"standard-version"})," for commit guidelines."]}),`
+`,e.jsxs(r.h3,{id:"068-2024-01-22",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.6.7...v0.6.8",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.6.8"})," (2024-01-22)"]}),`
+`,e.jsxs(r.h3,{id:"067-2024-01-22",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.6.6...v0.6.7",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.6.7"})," (2024-01-22)"]}),`
+`,e.jsx(r.h3,{id:"features",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," wip text field + 'more' component (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/6ba183ab36f1b6c79944f10846b7ac9d4268c985",target:"_blank",rel:"nofollow noopener noreferrer",children:"6ba183a"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"more component:"})," ioMore component is vertical ellispsis based on IoIconButton (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/0f77e49e49da5ed65ae02d00fd25b1e831933450",target:"_blank",rel:"nofollow noopener noreferrer",children:"0f77e49"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"more component:"})," wIP IoMore component (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/41c57cf3df94f850823c814d34e9e15d58f19553",target:"_blank",rel:"nofollow noopener noreferrer",children:"41c57cf"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"textfield conversion:"})," wIP IoTextField story (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/e61b9f922c49c01ba8cbf59f2db91bd378133078",target:"_blank",rel:"nofollow noopener noreferrer",children:"e61b9f9"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"textfield:"})," convert textfield to vuetify (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/a67e829c92a7c7febc864454f6a572289e0c8e59",target:"_blank",rel:"nofollow noopener noreferrer",children:"a67e829"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"066-2024-01-10",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.6.5...v0.6.6",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.6.6"})," (2024-01-10)"]}),`
+`,e.jsx(r.h3,{id:"features-1",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," select checkboxes (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/29394bc696a65d07d03910d7acaf3cf48d0e4805",target:"_blank",rel:"nofollow noopener noreferrer",children:"29394bc"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," slots working. edit in place text (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/3ec42d8d4b9cbb68cc2439ce559d1dd4c1a3022e",target:"_blank",rel:"nofollow noopener noreferrer",children:"3ec42d8"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," wIP adding drag/drop (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/43fc2389b127bc16ca5345ab16e32f82e955396b",target:"_blank",rel:"nofollow noopener noreferrer",children:"43fc238"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"065-2023-12-29",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.6.4...v0.6.5",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.6.5"})," (2023-12-29)"]}),`
+`,e.jsx(r.h3,{id:"features-2",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," datatable selected items, misc styling, table checkboxes WIP (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/4f0b8e9ee96003cda23d156e11ada4500e978be8",target:"_blank",rel:"nofollow noopener noreferrer",children:"4f0b8e9"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," wip. added checkboxes, misc styling fixes (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/45756412f23578825350be10a56120cca723582e",target:"_blank",rel:"nofollow noopener noreferrer",children:"4575641"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"064-2023-12-26",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.6.2...v0.6.4",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.6.4"})," (2023-12-26)"]}),`
+`,e.jsx(r.h3,{id:"features-3",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"chips:"})," add indicator chips and variants (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/de88e1ebad2467d9ce1c5f0220b55e2c4a8eace8",target:"_blank",rel:"nofollow noopener noreferrer",children:"de88e1e"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"chips:"})," added base indicator chips (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/51eae844618bb5164dadf662ced32b76106e68f6",target:"_blank",rel:"nofollow noopener noreferrer",children:"51eae84"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," get working in consumer (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/71b323c83ddd020b1de7777f2ac7e98be05d1e9d",target:"_blank",rel:"nofollow noopener noreferrer",children:"71b323c"}),")"]}),`
+`]}),`
+`,e.jsx(r.h3,{id:"bug-fixes",children:"Bug Fixes"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"storybook:"})," fix storybook not rendering chip for table (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/b5ddf56f184976053df22ecb52c9beb9bef2da23",target:"_blank",rel:"nofollow noopener noreferrer",children:"b5ddf56"}),")"]}),`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"storybook:"})," fix storybook not rendering preview (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/364c7098c049b77ff450a0e6e57c0427ff4d1c17",target:"_blank",rel:"nofollow noopener noreferrer",children:"364c709"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"062-2023-12-19",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.6.1...v0.6.2",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.6.2"})," (2023-12-19)"]}),`
+`,e.jsx(r.h3,{id:"features-4",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," add v-select (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/72726f5035dac2e30a5b06a54c0e8afa26bf4b52",target:"_blank",rel:"nofollow noopener noreferrer",children:"72726f5"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"061-2023-12-18",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.5.11...v0.6.1",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.6.1"})," (2023-12-18)"]}),`
+`,e.jsxs(r.h3,{id:"0511-2023-12-18",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.5.10...v0.5.11",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.5.11"})," (2023-12-18)"]}),`
+`,e.jsx(r.h3,{id:"features-5",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," datatable now displays in sb (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/d2bbcde13f3b1f5ff548163f669d0639afceba78",target:"_blank",rel:"nofollow noopener noreferrer",children:"d2bbcde"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"0510-2023-12-15",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.5.9...v0.5.10",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.5.10"})," (2023-12-15)"]}),`
+`,e.jsx(r.h3,{id:"features-6",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," test using chip in table (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/921c8b4d623c0a73a10ddab63119e7d85348fe41",target:"_blank",rel:"nofollow noopener noreferrer",children:"921c8b4"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"059-2023-12-15",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.5.8...v0.5.9",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.5.9"})," (2023-12-15)"]}),`
+`,e.jsx(r.h3,{id:"features-7",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatable:"})," fix host styles (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/cdeecf7af50f3a98670016624513c3defb361a6a",target:"_blank",rel:"nofollow noopener noreferrer",children:"cdeecf7"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"058-2023-12-06",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.4.0...v0.5.8",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.5.8"})," (2023-12-06)"]}),`
+`,e.jsxs(r.h2,{id:"040-2023-12-05",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.3.18...v0.4.0",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.4.0"})," (2023-12-05)"]}),`
+`,e.jsx(r.h3,{id:"-breaking-changes",children:"⚠ BREAKING CHANGES"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[`
+`,e.jsxs(r.p,{children:[e.jsx(r.strong,{children:"vue 3 > 2:"})," IODataTable may be broken"]}),`
+`]}),`
+`,e.jsxs(r.li,{children:[`
+`,e.jsxs(r.p,{children:[e.jsx(r.strong,{children:"vue 3 > 2:"})," test vue 2 component (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/9beb1af4b3b41fcd7549ce22c01fc384ec24427e",target:"_blank",rel:"nofollow noopener noreferrer",children:"9beb1af"}),")"]}),`
+`]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"0318-2023-12-04",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.3.17...v0.3.18",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.3.18"})," (2023-12-04)"]}),`
+`,e.jsx(r.h3,{id:"features-8",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"radio button:"})," add radio to package (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/e45a94d55d0b0d4f74fe6f9a79b108931cdb0419",target:"_blank",rel:"nofollow noopener noreferrer",children:"e45a94d"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"0317-2023-12-04",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.3.16...v0.3.17",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.3.17"})," (2023-12-04)"]}),`
+`,e.jsx(r.h3,{id:"features-9",children:"Features"}),`
+`,e.jsxs(r.ul,{children:[`
+`,e.jsxs(r.li,{children:[e.jsx(r.strong,{children:"datatables:"})," datatables use one test api (",e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/commit/fed6bc8127991a4d1baf67c32755a5d8c84a0d30",target:"_blank",rel:"nofollow noopener noreferrer",children:"fed6bc8"}),")"]}),`
+`]}),`
+`,e.jsxs(r.h3,{id:"0316-2023-12-01",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.3.15...v0.3.16",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.3.16"})," (2023-12-01)"]}),`
+`,e.jsxs(r.h3,{id:"0315-2023-12-01",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.3.13...v0.3.15",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.3.15"})," (2023-12-01)"]}),`
+`,e.jsxs(r.h3,{id:"0314-2023-12-01",children:[e.jsx(r.a,{href:"https://github.com/tejas-encora/IO-Component-Library/compare/v0.3.13...v0.3.14",target:"_blank",rel:"nofollow noopener noreferrer",children:"0.3.14"})," (2023-12-01)"]})]})}function i(n={}){const{wrapper:r}=Object.assign({},t(),n.components);return r?e.jsx(r,Object.assign({},n,{children:e.jsx(o,n)})):o(n)}export{i as default};
---
+++ assets/TextField-BBrstkXU.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{M as i,P as s,C as m}from"./index-skksH92X.js";import{T as p}from"./textfield.stories-8PtQlvr_.js";import{useMDXComponents as e}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-GdAhy-ac.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-Uk8hDnuP.js";import"./index-ogXoivrg.js";import"./index-yyIE76pf.js";import"./index-PPLHz8o0.js";import"./IoTextField-0ezx9CIp.js";import"./vue.esm-bundler-488J0-Su.js";import"./VTextField-rJC92YyY.js";import"./index-m0DKv_rT.js";/* empty css              */function n(o){const r=Object.assign({h1:"h1",p:"p",h2:"h2"},e(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(i,{of:p}),`
+`,t.jsx(r.h1,{id:"text-field",children:"Text Field"}),`
+`,t.jsx(r.p,{children:"Text fields allow users to enter text into a UI. They typically appear in forms and dialogs."}),`
+`,t.jsx(s,{}),`
+`,t.jsx(r.h2,{id:"props",children:"Props"}),`
+`,t.jsx(m,{})]})}function O(o={}){const{wrapper:r}=Object.assign({},e(),o.components);return r?t.jsx(r,Object.assign({},o,{children:t.jsx(n,o)})):n(o)}export{O as default};
