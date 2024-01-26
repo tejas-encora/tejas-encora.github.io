7a453b717417c4bde7c9f4b71d7eb9b72f6faa28
tejas-encora <tejas.monteverdi@encora.com>
2024-01-26 09:17:35 -0500

    New Build

---
+++ assets/Options-VM2xFwVa.js
@@ -0,0 +1,6 @@
+import{j as t}from"./jsx-runtime-vNq4Oc-g.js";import{M as s,P as m,C as p}from"./index-H9A1jbxN.js";import{O as e}from"./options.stories-M7EdzeUM.js";import{useMDXComponents as r}from"./index-_VGcpBFS.js";import"./index-4g5l5LRQ.js";import"./_commonjsHelpers-4gQjN7DL.js";import"./iframe-YrtrS7g3.js";import"../sb-preview/runtime.js";import"./chunk-ZGA76URP-Uk8hDnuP.js";import"./index-ogXoivrg.js";import"./index-yyIE76pf.js";import"./index-PPLHz8o0.js";import"./menu-Cce92um_.js";import"./animation-_TEqrmD1.js";import"./elevation-8D4iMx2P.js";import"./ripple-7WbymdBP.js";import"./query-assigned-elements-BJFWMYs6.js";import"./style-map-9Yerw4BC.js";import"./static-VcHqXR5s.js";import"./delegate-G9HsjGKO.js";import"./vue.esm-bundler-IQnt_YWr.js";function i(o){const n=Object.assign({h1:"h1",p:"p",h2:"h2"},r(),o.components);return t.jsxs(t.Fragment,{children:[t.jsx(s,{of:e}),`
+`,t.jsx(n.h1,{id:"options-menu",children:"Options Menu"}),`
+`,t.jsx(n.p,{children:"Ellipsis that displays menu on click."}),`
+`,t.jsx(m,{}),`
+`,t.jsx(n.h2,{id:"props",children:"Props"}),`
+`,t.jsx(p,{})]})}function w(o={}){const{wrapper:n}=Object.assign({},r(),o.components);return n?t.jsx(n,Object.assign({},o,{children:t.jsx(i,o)})):i(o)}export{w as default};
---
+++ assets/preview-IMpmZVKE.js
@@ -0,0 +1,7 @@
+import{_ as e}from"./iframe-YrtrS7g3.js";import"../sb-preview/runtime.js";var a={docs:{renderer:async()=>{let{DocsRenderer:r}=await e(()=>import("./DocsRenderer-NNNQARDV-PYOMBuSa.js"),__vite__mapDeps([0,1,2,3,4,5,6,7,8,9]),import.meta.url);return new r}}};export{a as parameters};
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["./DocsRenderer-NNNQARDV-PYOMBuSa.js","./chunk-HLWAVYOI-QFRiwmXm.js","./iframe-YrtrS7g3.js","./index-4g5l5LRQ.js","./_commonjsHelpers-4gQjN7DL.js","./index-H9A1jbxN.js","./chunk-ZGA76URP-Uk8hDnuP.js","./index-ogXoivrg.js","./index-yyIE76pf.js","./index-PPLHz8o0.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
